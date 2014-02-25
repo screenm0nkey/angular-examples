dd1cd771d55359cbad83e0986606a12a49264a10
Nick Lowman <digital@nicklowman.co.uk>
2014-02-25 09:44:02 +0000

    Added more tests

---
+++ Examples/app/examples/expressions/expressions.js
@@ -0,0 +1,8 @@
+/*
+egghead lesson 87
+*****************
+ng-click="todoCtrl.addTodo(newTodo)"
+ng-click="somearray.push({item:obj, done: false})"
+ng-click="TodoService.somearray.push({item:obj, done: false})"
+
+*/
